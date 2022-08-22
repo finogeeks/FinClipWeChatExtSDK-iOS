c49f6e78cd67d61535c29ea0adca476132f1504d
wangzhaoyao <wangzhaoyao@finogeeks.com>
2022-08-22 23:32:39 +0800

    1.update

--- FinAppletWXExt/FinAppletWXExt.h
+++ FinAppletWXExt/FinAppletWXExt.h
@@ -16,3 +16,4 @@ FOUNDATION_EXPORT const unsigned char FinAppletWXExtVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <FinAppletWXExt/PublicHeader.h>
 
 #import "FATWXExtComponent.h"
+#import "FATWXApiManager.h"
