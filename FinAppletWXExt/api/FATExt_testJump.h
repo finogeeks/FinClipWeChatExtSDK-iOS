c36227bb4e941e36903d45686b59985e3a730d4d
wangzhaoyao <wangzhaoyao@finogeeks.com>
2022-08-29 17:28:03 +0800

    1.update

---
+++ FinAppletWXExt/api/FATExt_testJump.h
@@ -0,0 +1,16 @@
+//
+//  FATExt_testJump.h
+//  FinAppletWXExt
+//
+//  Created by 王兆耀 on 2022/8/23.
+//
+
+#import "FATWXExtBaseApi.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FATExt_testJump : FATWXExtBaseApi
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FinAppletWXExt/api/FATExt_testJump.m
@@ -0,0 +1,63 @@
+//
+//  FATExt_testJump.m
+//  FinAppletWXExt
+//
+//  Created by 王兆耀 on 2022/8/23.
+//
+
+#import "FATExt_testJump.h"
+#import "WXApi.h"
+#import "WXApiObject.h"
+#import "FATWXApiManager.h"
+#import "FATWXUtils.h"
+@implementation FATExt_testJump
+
+- (void)setupApiWithCallback:(FATExtensionApiCallback)callback {
+    
+    FATAppletInfo *appInfo = [[FATClient sharedClient] currentApplet];
+    NSDictionary *info = appInfo.wechatLoginInfo;
+    
+    NSArray *extUrlsArray = info[@"extUrls"];
+    if ([FATWXUtils fat_isEmptyArrayWithArry:extUrlsArray]) {
+        if (callback) {
+            callback(FATExtensionCodeFailure,@{@"errMsg":@"path not exist"});
+        }
+        return;
+    }
+    NSString *pathString;
+    for (NSDictionary *dic in extUrlsArray) {
+        if ([dic[@"fieldName"] isEqualToString:@"test"]) {
+            pathString = dic[@"pageUrl"];
+        }
+    }
+    
+//    pathString = [NSString stringWithFormat:@"%@", info[@"test"]];
+    if ([FATWXUtils fat_isEmptyWithString:pathString]) {
+        if (callback) {
+            callback(FATExtensionCodeFailure,@{@"errMsg":@"path not exist"});
+        }
+        return;
+    }
+    WXLaunchMiniProgramReq *launchMiniProgramReq = [WXLaunchMiniProgramReq object];
+    launchMiniProgramReq.userName = info[@"wechatOriginId"];
+    launchMiniProgramReq.path = pathString;
+    if (appInfo.appletVersionType == FATAppletVersionTypeRelease) {
+        launchMiniProgramReq.miniProgramType = WXMiniProgramTypeRelease;
+    } else if (appInfo.appletVersionType == FATAppletVersionTypeTrial) {
+        launchMiniProgramReq.miniProgramType = WXMiniProgramTypePreview;
+    } else {
+        launchMiniProgramReq.miniProgramType = WXMiniProgramTypeTest;
+    }
+    [WXApi sendReq:launchMiniProgramReq completion:^(BOOL success) {
+        
+    }];
+    
+    [FATWXApiManager sharedManager].wxResponse = ^(WXLaunchMiniProgramResp *resp) {
+        NSDictionary *dic = [FATWXUtils dictionaryWithJsonString:resp.extMsg];
+        if (callback) {
+            callback([dic[@"errMsg"] containsString:@"fail"] ? FATExtensionCodeFailure : FATExtensionCodeSuccess, dic);
+        }
+    };
+}
+
+@end
