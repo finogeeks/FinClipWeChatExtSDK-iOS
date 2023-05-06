ac4102ab14af7180ee2979c1d81f0995aad8dd0f
jizelin <jizelin@finogeeks.club>
2023-05-06 15:13:10 +0000

    Stable dev

---
+++ FinAppletWXExt/api/FATExt_navigateToWechatMiniProgram.m
@@ -0,0 +1,51 @@
+//
+//  FATExt_navigateToWechatMiniProgram.m
+//  FinAppletWXExt
+//
+//  Created by 王兆耀 on 2023/4/24.
+//
+
+#import "FATExt_navigateToWechatMiniProgram.h"
+#import "WXApi.h"
+#import "WXApiObject.h"
+#import "FATWXApiManager.h"
+#import "FATWXUtils.h"
+
+@implementation FATExt_navigateToWechatMiniProgram
+
+- (void)setupApiWithSuccess:(void (^)(NSDictionary<NSString *, id> *successResult))success
+                    failure:(void (^)(NSDictionary *failResult))failure
+                     cancel:(void (^)(NSDictionary *cancelResult))cancel {
+    WXLaunchMiniProgramReq *launchMiniProgramReq = [WXLaunchMiniProgramReq object];
+    launchMiniProgramReq.userName = self.originId;
+    launchMiniProgramReq.path = self.path;
+    if ([self.envVersion isEqualToString:@"develop"]) {
+        launchMiniProgramReq.miniProgramType = WXMiniProgramTypeTest;
+    } else if ([self.envVersion isEqualToString:@"trial"]) {
+        launchMiniProgramReq.miniProgramType = WXMiniProgramTypePreview;
+    } else {
+        launchMiniProgramReq.miniProgramType = WXMiniProgramTypeRelease;
+    }
+    [WXApi sendReq:launchMiniProgramReq completion:^(BOOL success) {
+        if (!success) {
+            failure(@{});
+        }
+    }];
+    
+    [FATWXApiManager sharedManager].wxResponse = ^(WXLaunchMiniProgramResp *resp) {
+        NSDictionary *dic = [FATWXUtils dictionaryWithJsonString:resp.extMsg];
+        
+        BOOL result = [dic[@"errMsg"] containsString:@"fail"] ? NO : YES;
+        if (result) {
+            if (success) {
+                success(dic);
+            }
+        } else {
+            if (failure) {
+                failure(dic);
+            }
+        }
+    };
+}
+
+@end
--- Podfile
+++ Podfile
@@ -2,8 +2,7 @@ platform :ios, "9.0"
 source 'ssh://gitlab.finogeeks.club/finclip-ios/DevPods'
 source 'ssh://gitlab.finogeeks.club/finclip-ios/FinPods'
 source 'https://cdn.cocoapods.org/'
-#source 'https://github.com/CocoaPods/Specs.git' 
-
+use_frameworks!
 
 post_install do |installer|
     installer.pods_project.targets.each do |target|
@@ -34,7 +33,7 @@ inhibit_all_warnings!
 
 target "FinAppletWXExt" do
     project 'FinAppletWXExt.xcodeproj'
-    pod 'FinApplet','2.40.5'
+    pod 'FinApplet','2.40.6-dev20230506v09'
     pod 'WechatOpenSDK'
 end
 
