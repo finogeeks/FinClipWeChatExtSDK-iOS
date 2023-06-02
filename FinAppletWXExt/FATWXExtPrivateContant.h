7ee4c17e827483eebf3f8b8f3447157e9293cc56
luojian <luojian@finogeeks.com>
2023-06-02 14:28:54 +0000

    Stable dev

--- FinAppletWXExt/FATWXExtPrivateContant.h
+++ FinAppletWXExt/FATWXExtPrivateContant.h
@@ -8,6 +8,6 @@
 #ifndef FATWXExtPrivateContant_h
 #define FATWXExtPrivateContant_h
 
-static NSString *FATWXExtVersionString = @"2.40.9";
+static NSString *FATWXExtVersionString = @"2.40.10-dev20230602v09";
 
 #endif /* FATWXExtPrivateContant_h */
