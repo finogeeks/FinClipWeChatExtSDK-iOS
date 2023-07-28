fc14757ca401d6703616bbe8d5053967e1f47314
jenkins <jenkins@finogeeks.com>
2023-07-28 19:28:17 +0800

    update 2.41.4-dev20230728v10 code FinAppletWXExt.podspec

--- FinAppletWXExt/FATWXExtPrivateContant.h
+++ FinAppletWXExt/FATWXExtPrivateContant.h
@@ -8,6 +8,6 @@
 #ifndef FATWXExtPrivateContant_h
 #define FATWXExtPrivateContant_h
 
-static NSString *FATWXExtVersionString = @"2.41.4-dev20230728v08";
+static NSString *FATWXExtVersionString = @"2.41.4-dev20230728v10";
 
 #endif /* FATWXExtPrivateContant_h */
