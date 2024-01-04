828beb5d0bb7a1ac3cfefd8089d2ee84f48d4166
mac-studio <mac-studio@finogeeks.com>
2024-01-04 17:26:41 +0800

    update 2.43.0-dev20240104v08 code FinAppletWXExt.podspec

--- FinAppletWXExt/FATWXExtPrivateContant.h
+++ FinAppletWXExt/FATWXExtPrivateContant.h
@@ -8,6 +8,6 @@
 #ifndef FATWXExtPrivateContant_h
 #define FATWXExtPrivateContant_h
 
-static NSString *FATWXExtVersionString = @"2.43.0-dev20240104v01";
+static NSString *FATWXExtVersionString = @"2.43.0-dev20240104v08";
 
 #endif /* FATWXExtPrivateContant_h */
