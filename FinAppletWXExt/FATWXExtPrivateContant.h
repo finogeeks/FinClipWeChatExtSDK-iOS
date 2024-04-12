2bf95897268acac5d7ccd6456ba7508532a69521
mac-studio <mac-studio@finogeeks.com>
2024-04-12 20:40:09 +0800

    update 2.44.0-dev20240412v03 code FinAppletWXExt.podspec

--- FinAppletWXExt/FATWXExtPrivateContant.h
+++ FinAppletWXExt/FATWXExtPrivateContant.h
@@ -8,6 +8,6 @@
 #ifndef FATWXExtPrivateContant_h
 #define FATWXExtPrivateContant_h
 
-static NSString *FATWXExtVersionString = @"2.44.0-dev20240412v01";
+static NSString *FATWXExtVersionString = @"2.44.0-dev20240412v03";
 
 #endif /* FATWXExtPrivateContant_h */
--- Podfile
+++ Podfile
@@ -33,7 +33,7 @@ inhibit_all_warnings!
 
 target "FinAppletWXExt" do
     project 'FinAppletWXExt.xcodeproj'
-    pod 'FinApplet','2.44.0-dev20240412v01'
+    pod 'FinApplet','2.44.0-dev20240412v03'
     pod 'WechatOpenSDK'
 end
 
