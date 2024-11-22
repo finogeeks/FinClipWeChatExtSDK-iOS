b8a0f7d755b54a65f47ebe3266b5d8a77425bd6e
yangtao <yangtao@finogeeks.com>
2024-11-22 10:17:32 +0800

    update 2.46.6-dev20241122v01 code FinAppletWXExt.podspec

--- FinAppletWXExt/FATWXExtPrivateContant.h
+++ FinAppletWXExt/FATWXExtPrivateContant.h
@@ -8,6 +8,6 @@
 #ifndef FATWXExtPrivateContant_h
 #define FATWXExtPrivateContant_h
 
-static NSString *FATWXExtVersionString = @"2.46.6-dev20241121v01";
+static NSString *FATWXExtVersionString = @"2.46.6-dev20241122v01";
 
 #endif /* FATWXExtPrivateContant_h */
