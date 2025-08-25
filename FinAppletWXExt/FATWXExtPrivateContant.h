27cce2fcbeb05016c6d180ba7a511881f55f16fe
yangtao <yangtao@finogeeks.com>
2025-08-25 16:18:34 +0800

    update 2.49.6-dev20250825v02 code FinAppletWXExt.podspec

--- FinAppletWXExt/FATWXExtPrivateContant.h
+++ FinAppletWXExt/FATWXExtPrivateContant.h
@@ -8,6 +8,6 @@
 #ifndef FATWXExtPrivateContant_h
 #define FATWXExtPrivateContant_h
 
-static NSString *FATWXExtVersionString = @"2.49.6-dev20250825v01";
+static NSString *FATWXExtVersionString = @"2.49.6-dev20250825v02";
 
 #endif /* FATWXExtPrivateContant_h */
--- Podfile
+++ Podfile
@@ -33,7 +33,7 @@ inhibit_all_warnings!
 
 target "FinAppletWXExt" do
     project 'FinAppletWXExt.xcodeproj'
-    pod 'FinApplet','2.49.6-dev20250825v01'
+    pod 'FinApplet','2.49.6-dev20250825v02'
     pod 'WechatOpenSDK-XCFramework'
 end
 
